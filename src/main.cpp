93dcf08cae910deee30438ea175c3979cdf0c824
Jaal-09 <jaal9709+github@gmail.com>
2026-06-06 15:52:41 -0500

    Cambios en el main para mostrar el reloj y actualizar falta completar parte del main aun

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <Arduino.h>
 #include "PantallaManager.h"
 
+#include "RelojManager.h"
+
+// Instanciamos los objetos bajo arquitectura POO
 PantallaManager miPantalla;
+RelojManager miReloj;
 
 unsigned long tiempoUltimaActualizacion = 0;
-const unsigned long INTERVALO_TFT = 2500; 
-bool primeraActualizacion = true;
+unsigned long tiempoUltimoCambioVista = 0;
+
+ // El reloj se refresca cada segundo para ver los segundos avanzar
+const unsigned long INTERVALO_TFT = 1000;      
+
+ // Cada 7 segundos rota la vista entre pantalla 1 y 2
+const unsigned long INTERVALO_VISTA = 7000;    
+
+//bandera de control para limpiar el letrero de carga
+bool primeraActualizacion = true;  
 
 //Se inicia la pantalla tft
 void setup() {
@@ -14,11 +26,13 @@ void setup() {
     
     Serial.println("[SISTEMA] Iniciando TFT ...");
     miPantalla.iniciar();
+    miReloj.iniciar();
     
-    // Mostramos el texto de carga de forma temporal
-    miPantalla.mostrarTexto("Iniciando Panel TFT ...", 60, 100, 2, TFT_WHITE);
+    // Mostramos tu texto de carga de forma temporal
+    miPantalla.mostrarTexto("Iniciando Panel TFT ...", 60, 100, 2, TFT_WHITE, TFT_NAVY);
     delay(1500);
 }
+
 
 void loop() {
     if (millis() - tiempoUltimaActualizacion >= INTERVALO_TFT) {
