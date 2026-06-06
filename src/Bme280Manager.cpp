3223fa2f2f644aaff1c1f1196ff67a4b84213c5f
Jaal-09 <jaal9709+github@gmail.com>
2026-06-06 18:13:47 -0500

    se agregaron manejadores en caso de error al detectar el sensor bme-280

--- src/Bme280Manager.cpp
+++ src/Bme280Manager.cpp
@@ -15,5 +15,15 @@ bool Bme280Manager::iniciar() {
         return false;
     }
 
+    Serial.println("[BME280] Buscando sensor físico...");
+    if (!bme.begin(0x76, &Wire1)) {
+        Serial.println("[ERROR] ¡No se encontró el BME280! Revisa conexiones.");
+        sensorOperativo = false;
+        return false;
+    }
+    
+    Serial.println("[BME280] ¡Sensor detectado en pines 18 y 19 con éxito!");
+    sensorOperativo = true;
+    return true;
 
 }
