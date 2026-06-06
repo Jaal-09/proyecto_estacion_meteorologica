27397a61df34c13293eb384ea2e2bd1db234ee3e
Jaal-09 <jaal9709+github@gmail.com>
2026-06-05 19:21:05 -0500

    Inicializamos la pantalla tft, elegimos el tamaño de pixeles, le colocamos un banner y un titulo

--- src/PantallaManager.cpp
+++ src/PantallaManager.cpp
@@ -6,6 +6,21 @@ PantallaManager::PantallaManager() {
 }
 
 void PantallaManager::iniciar() {
+
     tft.init();
     tft.setRotation(1); 
+
+    tft.fillRect(0, 0, 320, 240, TFT_NAVY); 
+    delay(50);
+
+    //Banner
+    tft.fillRect(0, 0, 320, 38, TFT_BLUE);
+
+    // Configuración del título
+    tft.setTextColor(TFT_WHITE, TFT_BLUE);
+    tft.setTextSize(2);
+    tft.drawString("ESTACION METEOROLOGICA", 30, 10);
+
+
+
 }
