28b83ce7fa9dc666621c937f966a02359514efb1
Jaal-09 <jaal9709+github@gmail.com>
2026-06-05 19:48:49 -0500

    Se muestra un mensaje al iniciar la pantalla

--- src/main.cpp
+++ src/main.cpp
@@ -6,3 +6,16 @@ PantallaManager miPantalla;
 unsigned long tiempoUltimaActualizacion = 0;
 const unsigned long INTERVALO_TFT = 2500; 
 bool primeraActualizacion = true;
+
+//Se inicia la pantalla tft
+void setup() {
+    Serial.begin(115200);
+    delay(500);
+    
+    Serial.println("[SISTEMA] Iniciando TFT ...");
+    miPantalla.iniciar();
+    
+    // Mostramos el texto de carga de forma temporal
+    miPantalla.mostrarTexto("Iniciando Panel TFT ...", 60, 100, 2, TFT_WHITE);
+    delay(1500);
+}
