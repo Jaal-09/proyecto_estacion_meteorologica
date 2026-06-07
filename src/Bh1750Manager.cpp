8153ba90c75cd3ec6a5a0b59d2919bcfefab0f69
Jaal-09 <jaal9709+github@gmail.com>
2026-06-07 13:44:00 -0500

    Se agregaron comprobaciones para verificar que el sensor bh1750 este conectado y si es asi obtener los datos, se termino la implemnetacion del archivo manejador

--- src/Bh1750Manager.cpp
+++ src/Bh1750Manager.cpp
@@ -15,4 +15,24 @@ bool Bh1750Manager::iniciar() {
         Serial.println("[ERROR] No se pudo inicializar el bus I2C en pines 5 y 23.");
         return false;
     }
+
+    Serial.println("[LUZ] Buscando sensor BH1750 en pines 5 y 23...");
+
+    // Vinculamos el sensor al bus 'Wire' que acabamos de mover a los pines 5 y 23
+    if (!lightSensor.begin(0x23, &Wire)) {
+        Serial.println("[ERROR] ¡No se encontró el BH1750! Revisa pines 5/23 o energía.");
+        sensorOperativo = false;
+        return false;
+    }
+
+    Serial.println("[LUZ] ¡Sensor BH1750 detectado en pines 5 y 23 con éxito!");
+    sensorOperativo = true;
+    return true;
+}
+
+float Bh1750Manager::obtenerLuz() {
+    if (!sensorOperativo) return 0.0;
+
+    // Retorna el valor real en Lux
+    return lightSensor.readLightLevel(); 
 }
